9cb582b7be47de4811f05ba87dae297176a95c8d
XanxusX <tako30215@hotmail.com>
2017-12-07 11:53:06 +0800

    style with semantic

---
+++ src/components/Wall.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class='ui center aligned header'>Wall</h1>
+    <div class="ui container">
+      <div class="ui three column doubling grid">
+        <div class="column"><div class="ui top attached segment"><img class="ui fluid image" src="https://talk.pdis.nat.gov.tw/user_avatar/talk.pdis.nat.gov.tw/audreyt/800/574_1.png"></div><div class="ui bottom attached segment">content</div></div>
+        <div class="column"><div class="ui segment">Content</div></div>
+        <div class="column"><div class="ui segment">Content</div></div>
+        <div class="column"><div class="ui segment">Content</div></div>
+        <div class="column"><div class="ui segment">Content</div></div>
+        
+      </div> 
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Wall',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
